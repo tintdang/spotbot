91e5078472b0f335180002d5389dfd848d2efa7f
TinTDang <tin2dang@gmail.com>
2018-09-29 16:31:24 -0500

    added homepage and game routes with auth0

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Game from "./pages/Game";
 import Callback from "./pages/Callback";
+import Homepage from "./pages/Homepage";
 import './style.css';
 
 const App = props => {
-  const { isAuthenticated } = props.auth;
 
   return (
     <Router>
       <div>
         <Navbar />
         <Switch>
-          <Route exact path="/callback" render={() => (
+          <Route path="/callback" render={() => (
             <Callback auth={props.auth}/>
           )}/>
-          <Route exact path="/" component={Game} />
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/game" render={() => {
+            return (props.auth.isAuthenticated()) ? (
+              <Game auth={props.auth} {...props}/>
+            ) : (<Redirect to="/"/>)}}/>
+
         </Switch>
       </div>
     </Router>
