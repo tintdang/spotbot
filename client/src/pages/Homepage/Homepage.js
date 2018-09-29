ba89a5b1e30995a282f032694b0c8a5e52bb52de
TinTDang <tin2dang@gmail.com>
2018-09-29 15:58:54 -0500

    added homepage

---
+++ client/src/pages/Homepage/Homepage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Homepage = () =>
+
+  <div>
+    <h1>Welcome to the Game</h1>
+    <button>Click Here to Login (Button Currently not working)</button>
+  </div>;
+
+export default Homepage;
