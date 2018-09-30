034ac9503156b8576068ebcbd4689d2e771f3f6f
must-git-good <39744893+must-git-good@users.noreply.github.com>
2018-09-30 18:28:17 -0500

    user input boxes exist!

--- client/src/components/userSeat/userSeat.jsx
+++ client/src/components/userSeat/userSeat.jsx
@@ -19,16 +19,28 @@ class UserSeat extends React.Component {
         return (
             <div id="user-seat">
                 <div id="user-section">
-                    <div id="user-input">
-                        <textarea id="user-textbox">
-
-                        </textarea>
-                    </div>
                     <div id="user-info">
 
+                        <span>Welcome to SpotBot, 'Username'!</span><span>You've won # games.</span>
+
                     </div>
+
+                    <div id="user-input">
+
+                        <div className="input-group mb-3">
+                            <div className="input-group-prepend">
+                                <span className="input-group-text" id="basic-addon1">Convince us you're a bot:</span>
+                            </div>
+                            <input type="text" className="form-control" placeholder="Speak to us..." aria-label="user-input" aria-describedby="basic-addon1"></input>
+
+                        </div>
+
+                    </div>
+
+
                 </div>
             </div>
+
         )
     }
 }
