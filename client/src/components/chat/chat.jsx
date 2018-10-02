26cc303f0ff598cdb70b235d34c9bc8e98eaf4bd
must-git-good <39744893+must-git-good@users.noreply.github.com>
2018-10-02 14:21:33 -0500

    sdjlkf

--- client/src/components/chat/chat.jsx
+++ client/src/components/chat/chat.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import io from "socket.io-client";
 import './chat.css';
 
+const origin = window.location.origin;
 
 class Chat extends React.Component {
 
@@ -14,7 +15,7 @@ class Chat extends React.Component {
             messages: []
         };
 
-        this.socket = io('localhost:3001');
+        this.socket = io(origin);
 
         this.socket.on('RECEIVE_MESSAGE', function (data) {
             console.log("Received msg?", data);
