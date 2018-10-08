c5409a61aa8af628523e169221e40b0212938937
TDang <tin2dang@gmail.com>
2018-10-08 16:36:48 -0500

    THIS SHOULD WORK

--- client/src/pages/Game/Game.jsx
+++ client/src/pages/Game/Game.jsx
@@ -35,7 +35,7 @@ class Game extends React.Component {
 
         // USE THESE TO TOGGLE FOR PRODUCTION OR IMPLEMENT A SWITCH
         //this.socket = io(origin, {'sync disconnect on unload': true });
-        this.socket = io('localhost:3001', { 'sync disconnect on unload': true });
+        this.socket = io(origin, { 'sync disconnect on unload': true });
         // END PROD-SWITCH
 
         // receive chat messages from socket 
