c22811bc507adfecf7bdd3c9306c23f076a08f9a
TDang <tin2dang@gmail.com>
2018-10-07 22:18:22 -0500

    added function to kick users to our waiting room

--- client/src/pages/Game/Game.jsx
+++ client/src/pages/Game/Game.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Redirect } from 'react-router-dom';
 import './game.css';
 import UserSeat from '../../components/userSeat';
 import Navbar from "../../components/Navbar";
@@ -156,8 +157,9 @@ class Game extends React.Component {
             this.autoscrollDown()
         }
         setTimeout(() => {
-            // KICK PEOPLE
-        }, 1000);
+            // KICK PEOPLE to a broken page
+            this.props.history.push('/waitingroom')
+        }, 5000);
     }
 
     vote = value => {
