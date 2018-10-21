49a57567a5c23d72f02402e25af446054d8682ad
thmiv <thmiv4@gmail.com>
2018-10-20 19:30:45 -0500

    testing new designs on waitingroom

--- client/src/pages/Waitingroom/Waitingroom.jsx
+++ client/src/pages/Waitingroom/Waitingroom.jsx
@@ -19,21 +19,22 @@ class WaitingRoom extends React.Component {
             <div className="container-fluid land-bkgd">
                 <Navbar />
                 <div className="row justify-content-center">
-                
                     <div className="col-lg-8">
                         <div className="card" id="wait-card">
                             <h2 className="text-center wr" id="wait-card-title"> The Waiting Room: </h2>
                             {/* <hr id="wait-hr" /> */}
                             <h3 className="text-center wr">Hope you enjoyed that round!</h3>
                             <br />
-
                             <h4 className="text-center wr"><img alt="#" className="smallImg fl" id="wait-bot" src="/assets/images/noun_Chatbot_933467.png" />Pull up a chair and relax, or jump into a game!<br /><br /> Tired of sitting out while others play? Multiple game rooms: in an ice box near you! </h4><br />
                             {/* <h2>This is currently in p</h2> This will be for future development to check how many people are in the room*/}
-                            <button className="btn btn-dark" id="wait-btn-pad1" onClick={this.playAgain}> Play Again </button>
-                            <button className="btn btn-dark" id="wait-btn-pad2" onClick={this.logout}> Logout </button>
+                            <div className="row justify-content-center">
+                                <button className="btn btn-dark" id="wait-btn-pad1" onClick={this.playAgain}> Play Again </button>
+                            </div>
                         </div>
                     </div>
-                    
+                </div>
+                <div className="row justify-content-center">
+                    <button className="btn btn-dark" id="wait-btn-pad2" onClick={this.logout}> Logout </button>
                 </div>
             </div>
         )
